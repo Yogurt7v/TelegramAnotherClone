f2bda928f16aa5c12d496b645633b19b26fac48c
Yogurt7v <egorov-2k@yandex.ru>
2025-06-24 21:29:08 +0300

    first commit

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=TkHmVpyPvOU&t=421s&pp=0gcJCdAJAYcqIYzv
+
+7-01
---
+++ server/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import 'dotenv/config';
+import http from 'http';
+import cors from 'cors';
+
+const app = express();
+const server = http.createServer(app);
+const { PORT } = process.env || 4000;
+
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
